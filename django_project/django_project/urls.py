46b0c5c4dcbb00a24ed22fe93c429ff021894791
Syed Maisum <116006041+bulbinator@users.noreply.github.com>
2025-09-19 03:33:01 +0000

    login page

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("volunteers_r_us/", include('volunteers_r_us.urls'))
 ]
