c63566e275ffe9fdcce40b6d658d4f8311a33ce2
Syed Maisum <116006041+bulbinator@users.noreply.github.com>
2025-09-19 03:21:23 +0000

    created app

--- django_project/django_project/asgi.py
+++ django_project/django_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for volunteers_r_us project.
+ASGI config for django_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "volunteers_r_us.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")
 
 application = get_asgi_application()
