284f5393630e314a43bfdb36ff3dbabfb7475835
annauyentrang <annauyentrang@gmail.com>
2025-09-24 13:46:39 -0500

    Volunteer Display Notification for Matching

--- django_project/volunteers_r_us/urls.py
+++ django_project/volunteers_r_us/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.home, name='home'),
     path("login/", views.login, name='login'),
     path("register/", views.register, name='register'),
-    path("match/", views.match_volunteer, name="match_volunteer"),
+    path("match/", views.match_volunteer, name="match_volunteer")
+
 ]
