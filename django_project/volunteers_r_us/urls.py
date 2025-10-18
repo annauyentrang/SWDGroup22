d043e7cc43dce8de690af064748ad7ae77de9fac
Maralkrikorian <146392588+Maralkrikorian@users.noreply.github.com>
2025-10-17 21:23:20 -0500

    Update urls.py

--- django_project/volunteers_r_us/urls.py
+++ django_project/volunteers_r_us/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+app_name = "history"
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login_view, name='login'),
